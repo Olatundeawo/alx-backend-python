30013e57a96499a8c69a0f0856671413b5996552
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-07-06 08:15:30 -0500

    added the task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """a function that add two float parameters together"""
+    return a + b
