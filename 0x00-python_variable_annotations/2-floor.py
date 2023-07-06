14a4299a17baedaf5f11472f2dde54e2cbce894e
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-07-06 09:01:39 -0500

    modifty task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import math
 """
     Type annonation function
 """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """A function that takes an argument and
         return the floor of the arguments
     """
-    return (n)
+    return (math.floor(n))
