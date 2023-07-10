739c3e41ca9bd41a6660b3fdf9bf55b86b462161
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-07-10 11:56:40 -0500

    modify

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@
     and return the list of all the delays
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> int:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A function that import another function
     has two arguments and return a
