2f6a2bf490c7aed4541562d1b4e17494b0bf991b
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-07-11 10:35:46 -0500

    modifty task 1 and task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """
 A coroutine module
 """
-import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Function that uses async comprehension
     """
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,16 +8,14 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     A function that measure the runtime
     it was roughly 10 seconds because the
     range was set the 10
     """
     s = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(), async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(*(
+        async_comprehension() for i in range(4)))
     total = time.perf_counter() - s
     return total
